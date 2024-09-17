937d98a195ea51a05ba01e01da1340a00b91cb8c
msyafiqq <muhammad.syafiq@geniebook.com>
2024-09-17 15:15:11 +0700

    add pspdfkit

--- CanvasTest001/ViewController.swift
+++ CanvasTest001/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import PSPDFKit
+import PSPDFKitUI
 
 class ViewController: UIViewController {
 
@@ -15,5 +17,22 @@ class ViewController: UIViewController {
     }
 
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        // Update to use your document name.
+        let fileURL = Bundle.main.url(forResource: "blank", withExtension: "pdf")!
+        let document = Document(url: fileURL)
+
+        // The configuration closure is optional and allows additional customization.
+        let pdfController = PDFViewController(document: document) {
+            $0.isPageLabelEnabled = false
+        }
+
+        let nav = UINavigationController(rootViewController: pdfController)
+        nav.modalPresentationStyle = .fullScreen
+        // Present the PDF view controller within a `UINavigationController` to show built-in toolbar buttons.
+        present(nav, animated: true)
+    }
 }
 
